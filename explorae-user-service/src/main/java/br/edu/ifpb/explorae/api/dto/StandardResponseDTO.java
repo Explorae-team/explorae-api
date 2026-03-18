c68bf8485718b1bd31f6988124d66b7a7a53b77f
Joseph Nichollas <nichollascavalcante@gmail.com>
2026-03-18 11:45:54 -0300

    feat: finaliza setup inicial do backend (SDGEU-9)

---
+++ explorae-user-service/src/main/java/br/edu/ifpb/explorae/api/dto/StandardResponseDTO.java
@@ -0,0 +1,14 @@
+package br.edu.ifpb.explorae.api.dto;
+
+public record StandardResponseDTO<T>(
+    String message,
+    T data
+) {
+    public static <T> StandardResponseDTO<T> success(String message, T data) {
+        return new StandardResponseDTO<>(message, data);
+    }
+
+    public static <T> StandardResponseDTO<T> success(String message) {
+        return new StandardResponseDTO<>(message, null);
+    }
+}
